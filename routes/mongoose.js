227b2ed90096046a8e189c1fd19cab6639f72100
Léa <heilea@hotmail.fr>
2018-08-13 11:38:54 +0200

    git don t want to rollback so rewriting the content

--- routes/mongoose.js
+++ routes/mongoose.js
@@ -1,11 +1,7 @@
 var mongoose = require('mongoose');
 
 let uri;
-//uri = 'mongodb://lea:oxfam-lea-1@ds143971.mlab.com:43971/oxfam-test';
-// uri='https://696595.admin.sd5.gpaas.net/adminer.php?mongo=sftp.sd5.gpaas.net&username=696595&db=oxfam';
-// mongodb://localhost:27017/<database_name>
-
-uri="mongodb://localhost:27017/oxfam"
+uri = 'mongodb://lea:oxfam-lea-1@ds143971.mlab.com:43971/oxfam-test';
 mongoose.connect(uri,  { useNewUrlParser: true } )
 	.then(() => console.log('Connected to MongoDB ... '))
 	.catch(err => console.log('Could not connect to MongoDB ...', err));
