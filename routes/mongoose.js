1cc38106469aa4b2852f62f8294496139d361bde
Léa <heilea@hotmail.fr>
2018-08-13 11:11:13 +0200

    add adress to the database

--- routes/mongoose.js
+++ routes/mongoose.js
@@ -1,8 +1,11 @@
 var mongoose = require('mongoose');
 
 let uri;
-uri = 'mongodb://lea:oxfam-lea-1@ds143971.mlab.com:43971/oxfam-test';
+//uri = 'mongodb://lea:oxfam-lea-1@ds143971.mlab.com:43971/oxfam-test';
+// uri='https://696595.admin.sd5.gpaas.net/adminer.php?mongo=sftp.sd5.gpaas.net&username=696595&db=oxfam';
+// mongodb://localhost:27017/<database_name>
 
+uri='mongodb://696595:l5WSz@p6Qf3Y@localhost:27017/oxfam';
 mongoose.connect(uri,  { useNewUrlParser: true } )
 	.then(() => console.log('Connected to MongoDB ... '))
 	.catch(err => console.log('Could not connect to MongoDB ...', err));
